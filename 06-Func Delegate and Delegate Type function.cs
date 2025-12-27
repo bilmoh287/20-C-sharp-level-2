6572963713d2dfa5b51d5c666435269ed671b339
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-27 18:07:54 +0300

    06-Func delegate an delegate type function

---
+++ 06-Func Delegate and Delegate Type function.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    public class _06_Func_Delegate_and_Delegate_Type_function
+    {
+        class Program
+        {
+            //define a deligate type for int retuning(int signature)
+            delegate int CalcDelegate(int num);
+
+            //define a Func delegate to squere a number
+            static Func<int, int> Squere = SquereMethod;
+
+            //define a function to squere a number
+            static int SquereMethod(int num)
+            {
+                return num * num;
+            }
+
+            //define a function to sum a number
+            static int SumMethod(int num)
+            {
+                return num + num;
+            }
+
+            static void main()
+            {
+                int result;
+                //print using delegate type function
+                CalcDelegate calc = new CalcDelegate(SquereMethod);
+                result = calc(5);
+                Console.WriteLine("Function delegate Squere Result is : " + result);
+
+                CalcDelegate calc2 = new CalcDelegate(SumMethod);
+                result = calc2(5);
+                Console.WriteLine("Function delegate Sum Result is : " + result);
+
+                //print squere using Func
+                result = Squere(5);
+                Console.WriteLine("Func Squere Result is : " + result);
+            }
+
+        }
+    }
+}
