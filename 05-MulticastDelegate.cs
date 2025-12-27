5d0159e6094b42841e95979cd993fbc8f3cd9f7e
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-27 17:30:18 +0300

    05-MulticastDelegate

---
+++ 05-MulticastDelegate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    public class _05_MulticastDelegate
+    {
+        public delegate void MyDelegate(string message);
+
+
+        class Program
+        {
+            static void main()
+            {
+                MyDelegate myDelegate = Method1;
+                myDelegate += Method2;
+
+
+                myDelegate("Hello, world!");
+
+
+                myDelegate -= Method1;
+                myDelegate("Another message.");
+            }
+
+
+            static void Method1(string message)
+            {
+                Console.WriteLine("Method1: " + message);
+            }
+
+
+            static void Method2(string message)
+            {
+                Console.WriteLine("Method2: " + message);
+            }
+        }
+    }
+}
