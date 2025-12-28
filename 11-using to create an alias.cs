7bb4ad3fc1dd979d2ad57dc0b109d4fbd1e937cd
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-28 21:05:38 +0300

    using to create an alias

---
+++ 11-using to create an alias.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using koko = System.Console;
+
+namespace C_sharp_lever_2
+{
+    public class _11_using_to_create_an_aliad
+    {
+    class Program
+    {
+
+        static void Main()
+        {
+            koko.WriteLine("Hello World!");
+            koko.ReadLine();
+        }
+    }
+}
+}
