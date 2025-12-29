e2dd82a5ffb478956cfa46da73be650fddef2684
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-29 21:15:57 +0300

    Type Class

---
+++ 22-TypeClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    internal class _22_TypeClass
+    {
+        static void main()
+        {
+            Type myType = typeof(string);
+
+
+            Console.WriteLine("Type Information:");
+            Console.WriteLine($"Name: {myType.Name}");
+            Console.WriteLine($"Full Name: {myType.FullName}");
+            Console.WriteLine($"Is Class: {myType.IsClass}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
