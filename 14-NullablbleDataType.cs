c7981d5e097550ce6510f4e2c11fe318a0c9a13e
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-29 10:55:32 +0300

    14-NullablbleDataType

---
+++ 14-NullablbleDataType.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    class NullableExample
+    {
+        static void main()
+        {
+            // Declare a nullable int using Nullable<T>
+            Nullable<int> nullableInt1 = null;
+
+
+            // Shorthand notation using int?
+            int? nullableInt2 = null;
+
+
+            // Check if the nullable ints have values
+            if (nullableInt1.HasValue)
+            {
+                Console.WriteLine("nullableInt1 has a value: " + nullableInt1.Value);
+            }
+            else
+            {
+                Console.WriteLine("nullableInt1 is null.");
+            }
+
+
+            if (nullableInt2.HasValue)
+            {
+                Console.WriteLine("nullableInt2 has a value: " + nullableInt2.Value);
+            }
+            else
+            {
+                Console.WriteLine("nullableInt2 is null.");
+            }
+
+
+            // Using the null-coalescing operator
+            int result = nullableInt2 ?? 0;
+            Console.WriteLine("Using null-coalescing operator: " + result);
+
+
+            // Using the null-conditional operator
+            string stringValue = nullableInt2?.ToString();
+            Console.WriteLine("String representation: " + (stringValue ?? "null"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
