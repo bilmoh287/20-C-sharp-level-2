498ddc8cce35dbc48b499af13cc67d67a9af6042
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-29 19:40:53 +0300

    condtional attribute

---
+++ 20-CondittionalAttribue.cs
@@ -0,0 +1,42 @@
+﻿#define koko
+using System;
+using System.Diagnostics;
+
+
+public class MyClass
+{
+    [Conditional("DEBUG")]
+    public void DebugMethod()
+    {
+        Console.WriteLine("Debug method executed.");
+    }
+
+    public void NormalMethod()
+    {
+        Console.WriteLine("Normal method executed.");
+    }
+
+    [Conditional("koko")]
+    public static void LogTrace(string message)
+    {
+        Console.WriteLine($"[TRACE] {message}");
+    }
+}
+
+class Program2
+{
+    static void main()
+    {
+        MyClass myClass = new MyClass();
+
+
+        // Call the methods
+        myClass.DebugMethod();  // This will only be executed in DEBUG builds
+        myClass.NormalMethod(); // This will always be executed
+
+        MyClass.LogTrace("This trace message will only be included if TRACE_ENABLED is defined.");
+        Console.WriteLine("Rest of the program.");
+
+        Console.ReadLine();
+    }
+}
