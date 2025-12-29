495badae67bf36551c2d4fc31123aeb998144e76
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-29 20:04:32 +0300

    Obsolute Attribute

---
+++ 21-ObsoluteAtribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+public class MyClass2
+{
+    [Obsolete("This method is marked as obsolete, and will be deprecated in the future.")]
+    public void Method1()
+    {
+        Console.WriteLine("This method is marked as obsolete, and will be deprecated in the future.");
+    }
+
+
+    public void Method2()
+    {
+        Console.WriteLine("This is the recommended method to use.");
+    }
+}
+
+
+class Program3
+{
+    static void Main()
+    {
+        MyClass2 myObject = new MyClass2();
+
+
+        // Deprecated method usage
+        myObject.Method1(); // Generates a compiler warning
+
+
+        // New method usage
+        myObject.Method2();
+
+
+        Console.ReadLine();
+    }
+}
