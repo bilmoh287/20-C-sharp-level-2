dd85b3ba677256a657ff6bcba47ac17a3414557a
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-29 11:02:14 +0300

    Nullable Example

---
+++ 15-NullableDataTypeExamplecs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    public class _15_NullableDataTypeExamplecs
+    {
+        static void Procedure1(string Name, Nullable<int> Age)
+        {
+            Console.WriteLine("\nName is :" + Name);
+
+            if (Age.HasValue)
+            {
+                Console.WriteLine("Age is :" + Age.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Age is Null");
+            }
+        }
+
+        static void Procedure2(string Name, int? Age)
+        {
+            Console.WriteLine("\nName is :" + Name);
+
+            if (Age.HasValue)
+            {
+                Console.WriteLine("Age is :" + Age.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Age is Null");
+            }
+        }
+
+        static void main(string[] args)
+        {
+
+            Procedure1("Mohammed Abu-Hadhoud", null);
+            Procedure1("Ali Ahmed", 35);
+
+
+            Procedure2("Mohammed Abu-Hadhoud", null);
+            Procedure2("Ali Ahmed", 35);
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
