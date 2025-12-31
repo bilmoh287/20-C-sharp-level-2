58e5ff6ce28ac4a928be78372dac56da3dfb5736
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-31 15:29:22 +0300

    Generic Classes

---
+++ 28-GenericClasses.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    public class _28_GenericClasses
+    {
+        public class GenericBox<T>
+        {
+            private T _value;
+
+            public GenericBox(T value)
+            {
+                _value = value;
+            }
+
+            public T GetContent()
+            {
+                return _value;
+            }
+        }
+
+
+        class Program
+        {
+            static void Main()
+            {
+                // Usage:
+                GenericBox<int> intBox = new GenericBox<int>(42);
+                Console.WriteLine("Content of intBox: " + intBox.GetContent()); // Outputs: 42
+
+
+                GenericBox<string> stringBox = new GenericBox<string>("Free Palastine!");
+                Console.WriteLine("Content of stringBox: " + stringBox.GetContent()); // Outputs: Free Palastine!
+            }
+        }
+    }
+}
