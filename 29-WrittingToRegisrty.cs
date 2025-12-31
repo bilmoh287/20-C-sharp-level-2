ebf8d979d6c6a653b920e37794d9e532ded98220
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-31 15:55:05 +0300

    writting to registry

---
+++ 29-WrittingToRegisrty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace C_sharp_lever_2
+{
+    internal class _29_WrittingToRegisrty
+    {
+        class Program
+        {
+            static void Main()
+            {
+                // Specify the Registry key and path
+                string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\YourSoftware";
+                string valueName = "YourValueName";
+                string valueData = "YourValueData";
+
+
+                try
+                {
+                    // Write the value to the Registry
+                    Registry.SetValue(keyPath, valueName, valueData, RegistryValueKind.String);
+
+
+                    Console.WriteLine($"Value {valueName} successfully written to the Registry.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+    }
+}
