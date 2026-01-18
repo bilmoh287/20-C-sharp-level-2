4e024befdbfd00734f7924195b74215a0952ca69
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2026-01-18 21:17:08 +0300

    Asynchronous example using Task class

---
+++ 42-AynchronousTaskClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    public class _42_AynchronousTaskClass
+    {
+        static async Task Main()
+        {
+
+            // Create and run an asynchronous task
+            Task<int> resultTask = PerformAsyncOperation();
+
+            // Do some other work while waiting for the task to complete
+            Console.WriteLine("Doing some other work...");
+
+            int result = await resultTask;
+            Console.WriteLine($"Result: {result}");
+            Console.ReadLine();
+        }
+
+        static async Task<int> PerformAsyncOperation()
+        {
+            // Simulate an asynchronous operation
+            await Task.Delay(2000);
+
+            return 42;
+        }
+    }
+}
