5f405c8dfd4538c372d58a70415d28d0285b937d
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2026-01-18 21:35:20 +0300

    Async example

---
+++ 43-AynchronouesExample2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lever_2
+{
+    public class _43_AynchronouesExample2
+    {
+        static async Task Main()
+        {
+
+            Console.WriteLine("Starting tasks...");
+
+            // Start the first task to download and print content length from CNN
+            Task task1 = DownloadAndPrintAsync("https://www.cnn.com");
+            Console.WriteLine("Task 1 started...");
+
+            // Start the second task to download and print content length from Amazon
+            Task task2 = DownloadAndPrintAsync("https://www.amazon.com");
+            Console.WriteLine("Task 2 started...");
+
+            // Start the third task to download and print content length from ProgrammingAdvices
+            Task task3 = DownloadAndPrintAsync("https://www.ProgrammingAdvices.com");
+            Console.WriteLine("Task 3 started...\n");
+
+            // Wait for all tasks to complete
+            await Task.WhenAll(task1, task2, task3);
+
+            // Print a message indicating that all tasks have finished execution
+            Console.WriteLine("\nDone, all tasks finished execution.");
+            Console.ReadKey();
+        }
+
+        static async Task DownloadAndPrintAsync(string url)
+        {
+
+            string content;
+
+            // Using statement ensures that the WebClient is disposed of properly
+            using (WebClient client = new WebClient())
+            {
+                // Simulate some work by adding a delay
+                await Task.Delay(100);
+
+                // Download the content of the web page asynchronously
+                content = await client.DownloadStringTaskAsync(url);
+            }
+
+            // Print the URL and the length of the downloaded content
+            Console.WriteLine($"{url}: {content.Length} characters downloaded");
+        }
+    }
+}
