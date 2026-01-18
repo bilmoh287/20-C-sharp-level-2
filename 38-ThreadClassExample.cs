98714e61eae0d07ef09d811d909f74c17922df3d
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2026-01-18 17:22:35 +0300

    Thread class example

---
+++ 38-ThreadClassExample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace C_sharp_lever_2
+{
+    public class _38_ThreadClassExample
+    {
+        static void Main()
+        {
+            // Note that your program is the main thread.
+
+
+            // Create a new thread and start it
+            Thread t = new Thread(ThreadMethod1);
+            t.Start();
+
+            // Create a new thread and start it
+            Thread t2 = new Thread(ThreadMethod2);
+            t2.Start();
+
+            // Make the Main thread wait untill the child thread finishes
+            t.Join();
+            t2.Join();
+
+            // Main thread continues its execution
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Main Thread: " + i);
+                Thread.Sleep(1000); // Sleep for 1 second
+            }
+            Console.ReadKey();
+        }
+
+
+        static void ThreadMethod1()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Thread Method1: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void ThreadMethod2()
+        {
+            for (int i = 5; i >= 1; i--)
+            {
+                Console.WriteLine("Thread Method2: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
